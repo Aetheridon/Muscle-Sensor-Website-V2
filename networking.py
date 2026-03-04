dc36605c05d654c3b2cb0e4c82180dd42047818d
Aetheridon <lewisvr@protonmail.com>
2026-03-04 18:55:08 +0000

    UDP client connects to arduino and gets sent mock packets

--- networking.py
+++ networking.py
@@ -1,7 +1,6 @@
 ## Networking code
 import socket
-import threading
-import time
+import struct
 
 def connect(HOST, PORT):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,5 +9,6 @@ def connect(HOST, PORT):
     print("Registered with server, waiting for numbers...")
 
     while True:
-        data, addr = client.recvfrom(1024)
-        print(f"From {addr}: {data.decode().strip()}")
+        data, addr = client.recvfrom(4096) # 4096b is max, recvfrom will only return the actual size packet
+        value = struct.unpack("i", data)[0]
+        print(value)
