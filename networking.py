212179f62f345925ae9520c944fcc5f01fdb545d
Aetheridon <lewisvr@protonmail.com>
2026-03-12 13:21:42 +0000

    Convert analog to voltages

--- networking.py
+++ networking.py
@@ -28,12 +28,12 @@ def connect(HOST, PORT, latest_data, stop_event):
     while not stop_event.is_set():
         data, addr = client.recvfrom(4096) # 4096b is max, recvfrom will only return the actual size packet
         
-        sensorA0, sensorA1 = struct.unpack("<ii", data) # <ii defines how the bytes are structured
-        
+        sensorA0, sensorA1 = struct.unpack("<ff", data) # <ii defines how the bytes are structured
+
         latest_data["sensorA0"] = sensorA0
         latest_data["sensorA1"] = sensorA1
         
-        print(f"A0: {latest_data["sensorA0"]}, A1: {latest_data["sensorA1"]}")
+        print(f"A0: {latest_data["sensorA0"]:.3f}, A1: {latest_data["sensorA1"]:.3f}")
     
     client.sendto(b"STOP", (HOST, PORT))
     client.close()
